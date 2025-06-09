c65342319cfec1c3321ff4b80a985da656cf1145
Hajira-Rafiqa <60015164+Hajira-Rafiqa@users.noreply.github.com>
2025-06-09 11:58:37 +0530

    Add files via upload

---
+++ src/actions/alertAction.js
@@ -0,0 +1,15 @@
+
+export const setMsg = (msg, msgType) => {
+    const alert = {
+        msg, msgType
+    }
+    localStorage.setItem("alert", JSON.stringify(alert))
+}
+export const getMsg = () => {
+    if (localStorage.getItem("alert")) {
+        const alert = JSON.parse(localStorage.getItem("alert"))
+        localStorage.removeItem("alert")
+        return alert
+    }
+    return null
+}
---
+++ src/actions/type.js
@@ -0,0 +1,4 @@
+export const SET_AUTH = "SET_AUTH"
+export const CLEAR_AUTH = "CLEAR_AUTH"
+
+export const SET_TAB = "SET_TAB"
